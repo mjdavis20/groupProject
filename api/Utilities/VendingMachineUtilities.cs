74d1ad216becef94c5cc52b210ef070514731e90
Ryan Carlson <ryancarlson@Ryans-MacBook-Air-3.local>
2023-11-28 21:44:01 -0600

    Vending Machine Utility Done

--- api/Utilities/VendingMachineUtilities.cs
+++ api/Utilities/VendingMachineUtilities.cs
@@ -5,22 +5,44 @@ namespace api.Utilities
 {
     public class VendingMachineUtilities
     {
-         public List<VendingMachine> GetAllPurchaseEvents()
+        public List<VendingMachine> GetAllVendingMachines(bool Deleted)
         {
             ConnectionString myConnection = new ConnectionString();
             string cs  = myConnection.cs;
             using var con = new MySqlConnection(cs);
             con.Open();
-            string stm = "SELECT * FROM Vending Machines ORDER BY VendID ASC";
+            string stm = "SELECT * FROM Vending Machines WHERE Deleted = 0 ORDER BY VendID ASC";
             using var cmd = new MySqlCommand(stm, con);
-            MySqlDataReader rdr = cmd.ExecuteReader();
+            cmd.Parameters.AddWithValue("@Deleted", Deleted);
+            cmd.Prepare();
+            using MySqlDataReader rdr = cmd.ExecuteReader();
             List<VendingMachine> Allvendingmachines = new List<VendingMachine>();
             while(rdr.Read())
             {
-                Allvendingmachines.Add(new VendingMachine(){VendID = rdr.GetInt32(0), Address = rdr.GetString(1), ZipCode = rdr.GetInt32(2)});
+                Allvendingmachines.Add(new VendingMachine(){VendID = rdr.GetInt32(0), Address = rdr.GetString(1), ZipCode = rdr.GetInt32(0)});
             }
             con.Close();
-            return Allvendingmachines;
+            return VendingMachine;
+        }
+
+        public void NewVendingMachines(VendingMachine myVendingMachine)
+        {
+            ConnectionString myConnection = new ConnectionString();
+            string cs  = myConnection.cs;
+            using var con = new MySqlConnection(cs);
+            con.Open();
+            Console.WriteLine("New Vending Machine");
+
+            string stm = @"INSERT INTO Vending Machines(Address, ZipCode) VALUES(@Address, @ZipCode)";
+
+            using var cmd = new MySqlCommand(stm, con);
+
+            cmd.Parameters.AddWithValue("@Address", myVendingMachine.Address);
+            cmd.Parameters.AddWithValue("@ZipCode", myVendingMachine.ZipCode);
+
+            cmd.Prepare();
+
+            cmd.ExecuteNonQuery();
         }
     }
 }
