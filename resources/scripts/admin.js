e6ccadd6fd2256c5c98bbbf9f1db13a75c94e096
dpjefferson1 <dpjefferson1@crimson.ua.edu>
2023-11-30 18:00:21 -0600

    initial commit

--- resources/scripts/admin.js
+++ resources/scripts/admin.js
@@ -1,66 +1,77 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const itemInventory = getItemInventory(); 
+document.addEventListener("DOMContentLoaded", async function () {
+    const itemInventory = await getItemInventory();
 
-    // change to fit updated design doc
     // Populate the table with item inventory data
     const tableBody = document.querySelector("tbody");
     itemInventory.forEach(item => {
         const row = document.createElement("tr");
         row.innerHTML = `
-            <td>${item.code}</td>
-            <td id="inventory-${item.code}">${item.quantity}</td>
+            <td>${product.code}</td>
+            <td id="inventory-${product.code}">${product.quantity}</td>
             <td>
-                <button class="btn btn-success" onclick="addToInventory('${item.code}')">Add</button>
-                <button class="btn btn-danger" onclick="removeFromInventory('${item.code}')">Remove</button>
+                <button class="btn btn-success" onclick="addToInventory('${product.code}')">Add</button>
+                <button class="btn btn-danger" onclick="removeFromInventory('${product.code}')">Remove</button>
             </td>
         `;
         tableBody.appendChild(row);
     });
 });
 
-function addToInventory(itemCode) {
-    // logic to add inventory for the specified item
-    const itemInventory = getItemInventory();
-    const item = itemInventory.find(item => item.code === itemCode);
-    if (item) {
-        item.quantity += 1;
-        document.querySelector(`#inventory-${itemCode}`).textContent = item.quantity;
-        updateItemInventory(itemInventory);
+async function addToInventory(productId) {
+    try {
+        const itemInventory = await getItemInventory();
+        const product = itemInventory.find(item => product.code === itemCode);
+        if (product) {
+            product.quantity += 1;
+            document.querySelector(`#inventory-${productId}`).textContent = product.quantity;
+            await updateItemInventory(itemInventory);
+        }
+    } catch (error) {
+        console.error('Error adding to inventory:', error);
     }
 }
 
-function removeFromInventory(itemCode) {
-    // logic to remove inventory for the specified item
-    const itemInventory = getItemInventory();
-    const item = itemInventory.find(item => item.code === itemCode);
-    if (item && item.quantity > 0) {
-        item.quantity -= 1;
-        document.querySelector(`#inventory-${itemCode}`).textContent = item.quantity;
-        updateItemInventory(itemInventory);
+async function removeFromInventory(productId) {
+    try {
+        const itemInventory = await getItemInventory();
+        const item = itemInventory.find(item => product.code === itemCode);
+        if (item && item.quantity > 0) {
+            item.quantity -= 1;
+            document.querySelector(`#inventory-${productId}`).textContent = product.quantity;
+            await updateItemInventory(itemInventory);
+        }
+    } catch (error) {
+        console.error('Error removing from inventory:', error);
     }
 }
 
-function getItemInventory() {
-    // change to get from api
-    const itemInventoryStr = localStorage.getItem('itemInventory');
-    return itemInventoryStr ? JSON.parse(itemInventoryStr) : getDefaultItemInventory();
+async function getItemInventory() {
+    try {
+        const response = await fetch('/api/ProductRoute'); // Replace with your actual API endpoint
+        const itemInventory = await response.json();
+        return itemInventory;
+    } catch (error) {
+        console.error('Error fetching item inventory from API:', error);
+        return getDefaultItemInventory(); // Fallback to default inventory in case of an error
+    }
 }
 
-function updateItemInventory(itemInventory) {
-    // Update the item inventory in local storage
-    localStorage.setItem('itemInventory', JSON.stringify(itemInventory));
+async function updateItemInventory(itemInventory) {
+    try {
+        // Assuming your API supports updating the entire inventory
+        const response = await fetch('/api/ProductRoute', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(itemInventory),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Failed to update item inventory. Status: ${response.status}`);
+        }
+    } catch (error) {
+        console.error('Error updating item inventory:', error);
+    }
 }
 
-// remove
-function getDefaultItemInventory() {
-    // Define the default item inventory
-    return [
-        { code: "A1", quantity: 10 },
-        { code: "A2", quantity: 5 },
-        { code: "A3", quantity: 8 },
-        { code: "B1", quantity: 3 },
-        { code: "B2", quantity: 12 },
-        { code: "B3", quantity: 9 },
-        // Add more items and their inventory data as needed
-    ];
-}
